ba575e5211acb171ad9636a64e5dd5b1f87b0ec2
Taneli Nyyssölä <Taneli.Nyyssola96@hotmail.com>
2021-03-26 15:03:46 +0200

    amplify testi

--- musakorneri/src/index.js
+++ musakorneri/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import store from './store'
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <Provider store={store}>
